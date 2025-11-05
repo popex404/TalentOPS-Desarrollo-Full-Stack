1ad898200a8f774a0cf229b61e3113e39a933d9e
JavierGarcia <javiergarciapinilla95@gmail.com>
2025-11-05 17:52:41 -0300

    feat: agregar función de saludo personalizado

--- 1 Fundamentos JavaScript/1 Introducción y GitHub/Dia 3/practica-git-remoto/app.js
+++ 1 Fundamentos JavaScript/1 Introducción y GitHub/Dia 3/practica-git-remoto/app.js
@@ -1,1 +1,10 @@
 console.log("¡Hola desde GitHub!")
+
+// Función de saludo personalizado
+function saludarUsuario(nombre) {
+    console.log(`¡Hola, ${nombre}! Bienvenido a nuestro proyecto.`);
+}
+
+// Uso de la función
+saludarUsuario("Desarrollador");
+
